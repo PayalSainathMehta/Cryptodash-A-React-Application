6abd1d45b3140da2c15f8fbc8bc3dc1d059c5037
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-05-26 13:54:06 -0400

    Add Styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import WelcomeMessage from "./WelcomeMessage";
+import styled, {css} from "styled-components";
 
+const MyButton = styled.div`
+  color: green;
+  // ${props => props.primary && css`
+  //       color: palevioletred;
+  // ` }
+`
+
+// const TomatoButton = styled(MyButton)`
+//     color: tomato;
+//     border-color: tomato;
+// `
 function Index() {
   return (
-      <div>Hello World!</div>
+      <div>
+        <WelcomeMessage/>
+        {/*<MyButton>Hello</MyButton>*/}
+        {/*<MyButton primary>Hello</MyButton>*/}
+        {/*<TomatoButton>Hello</TomatoButton>*/}
+      </div>
   );
 }
 
