4cd752271d1b5d06c5a9d9f3f98ce2c3279a74a9
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-05-25 23:42:35 -0400

    Restructure files - remove boilerplate

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function Index() {
+  return (
+      <div>Hello World!</div>
+  );
+}
+
+export default Index;
