bf5f74ad4246f52f3aeb40f8010255907188a28f
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-05-28 13:41:23 -0400

    Add Coin Style

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import WelcomeMessage from "./WelcomeMessage";
 import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 import {AppProvider} from "./AppProvider";
-import Settings from "../Settings";
 import Content from "../Shared/Content";
+import Settings from "../Settings";
+import WelcomeMessage from "../Settings/WelcomeMessage";
 // import styled, {css} from "styled-components";
 
 // const MyButton = styled.div`
@@ -27,6 +27,7 @@ function Index() {
             <AppBar/>
                <Content>
                  <Settings/>
+                 {/*Welcome to CryptoDash!*/}
                </Content>
         </AppProvider>
       </AppLayout>
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,18 +1,24 @@
 import React from "react";
 import {AppContext} from "../App/AppProvider";
 import styled, {css} from "styled-components";
-import {SelectableTile} from "../Shared/Tile";
+import CoinTile from "./CoinTile";
+
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
 `
+
+function getCoinsToDisplay(coinList){
+    return Object.keys(coinList).slice(0,100);
+}
 export default function (props) {
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled>
-            {Object.keys(coinList).map(coinKey => <SelectableTile> {coinKey} </SelectableTile>)}
-        </CoinGridStyled>
-        }
+            {getCoinsToDisplay(coinList).map(coinKey =>
+                <CoinTile coinKey = {coinKey} />
+            )}
+        </CoinGridStyled>}
     </AppContext.Consumer>;
 }
