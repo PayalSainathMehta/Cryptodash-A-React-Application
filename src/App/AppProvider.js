9dea0b599ec1dc7489666aee8e926c7dc89c0a76
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-05-30 13:07:25 -0400

    Fetch prices data

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -40,6 +40,7 @@ export class AppProvider extends React.Component {
     }
         componentDidMount = () => {
         this.fetchCoins();
+        this.fetchPrices();
     }
 
     fetchCoins = async() => {
@@ -47,11 +48,34 @@ export class AppProvider extends React.Component {
         this.setState({coinList});
         // console.log(coinList);
     }
+
+    fetchPrices = async() => {
+        if(this.state.firstVisit) return;
+        let prices = await this.prices();
+        prices = prices.filter(price => Object.keys(price).length);
+        this.setState({prices});
+    }
+
+    prices = async() => {
+        let returnData = [];
+        for(let i = 0; i < this.state.favorites.length; i++){
+            try{
+                let priceData = await cc.priceFull(this.state.favorites[i],'USD');
+                returnData.push(priceData);
+            }
+            catch(e){
+                console.log("Fetch price error", e);
+            }
+        }
+        return returnData;
+    }
     confirmFavorites = () =>{
         // console.log("Helloooo");
         this.setState({
             firstVisit: false,
             page: 'dashboard'
+        }, () => {
+            this.fetchPrices();
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
             // test: 'helloo'
